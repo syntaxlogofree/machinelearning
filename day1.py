28460c630cfa1f0dbbf0d85aad5756a92252b944
syntaxlogofree <hello@mouthoftiger.com>
2017-01-25 23:16:54 -0500

    update

--- day1.py
+++ day1.py
@@ -24,3 +24,40 @@ name = "john"
 age = 35
 
 print "make up a sentence %s, and %d" % (name, age)
+
+x = "there are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+print "I said: %r" % x
+print "I also said: '%s'." % y
+
+# %r is best for debugging, raw data of the variable, the others are used for displaying to users.
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "string 1",
+    "string 2",
+    "string 3",
+    "string 4"
+)
+
+# use %s to print Chinese
+
+months = "Jan\nFeb\nMarch"
+
+print months
+
+print """
+Jan
+Feb
+March
+"""
+
+# \n or three quotes = <br/>
